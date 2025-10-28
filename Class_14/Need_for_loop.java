93e14b044c572433cce4f2879d3e80cf909a0094
Mubarak-Ansari <mubarakansari8948@gmail.com>
2025-10-28 08:53:18 +0530

    Need_for_loop_in_Java

---
+++ Class_14/Need_for_loop.java
@@ -0,0 +1,34 @@
+/*
+ *  In this lecture we will learn:
+What are loops in Java?
+Different kinds of loops
+Need for loops in Java
+
+#1
+Loops in Java is a feature used to execute a particular part of the program repeatedly if a given condition evaluates to be true.
+We use loops, when we have to execute a same statement multiple times.
+In Java, we have different types of loops:-
+1. while loop
+2. do while loop
+3. for loop 
+Different type of loops have different kind of use- cases like a finite number of times or based on some condition.
+
+#2
+Need of loops in Java:-
+It reduce the size of the code.
+Loops make an easy flow of the control.
+Loops reduce the time complexity and space complexity.
+ */
+
+ public class Need_for_loop{
+
+    public static void main(String args[]){
+
+        //repeat this statement 5 times
+        // concept of loops comes here
+        // loop - while, do while, for
+        System.out.println("hello g");
+    }
+ }
+
+ // next we will discuss in Class 15..
