c1906871cac20826fd6c832c8c3cef1c1e53eef9
Mubarak-Ansari <mubarakansari8948@gmail.com>
2026-01-01 17:52:13 +0530

    Inheritance

--- Class_49/Inheritance.java
+++ Class_49/Inheritance.java
@@ -28,6 +28,17 @@ A subclass does not inherit the private members of its parent class. *
 
 //package Class_49;
 
+class Calc{
+
+    public int add(int n1, int n2){
+        return n1 + n2;
+    }
+
+    public int sub(int n  1, int n2){
+        return n1 - n2;
+    }
+}
+
 public class Inheritance {
     public static void main(String args[]){
         System.out.println("Inheritance in Java");
