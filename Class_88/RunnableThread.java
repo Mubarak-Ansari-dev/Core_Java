e8d97c61c04295f1ec7f3ec44ec42c1557bd91e1
Mubarak-Ansari <mubarakansari8948@gmail.com>
2026-01-19 20:46:24 +0530

    Runnable vs Thread in Java

---
+++ Class_88/RunnableThread.java
@@ -0,0 +1,124 @@
+
+        /*
+        n this lecture we will learn:
+        Using thread through Runnable interface
+        How to start a thread with a Runnable interface
+        Difference between extending a thread and implementing a runnable interface
+        Use of anonymous class with runnable interface
+        Creating a thread with lambda expression
+
+
+        #1
+        Multiple Inheritance is not supported by Java. So, extending a thread is not a good practice to follow.
+        Thread is a class that implements Runnable and Runnable contains a method known as the run() method.
+        Instead of extending a thread, we can also implement it through an interface called Runnable.
+        class A implements Runnable
+        {
+        public void run()
+        {
+        statements;
+        }
+        }
+
+        #2
+        In the Runnabe method, the start() method is not present so we can not use it by implementing Runnable simply.
+        Thread has multiple constructors and one of the constructors takes a runnable object.
+        We cannot create an object of a thread by using a class name.
+        Objects for a thread will be created by using a Runnable keyword. So, we create a reference of an interface and an object of a class
+        e.g., Runnable obj= new A();
+        We have to pass a reference to an object in the thread class.
+        After creating a reference of the Runnable class, we can use the start() method with the thread.
+
+        #3
+        We can create a thread by using two methods:
+        1. Extend a thread class
+        2. Implement a Runnable interface
+        The runnable interface does not have thread methods, in that case, we need to create a separate thread object to use features.
+
+        #4 
+        We can also instantiate a runnable interface by using an anonymous class.
+        Runnable is a functional interface so we also use lambda expression with it.
+        */
+
+
+//package Class_88;
+
+// class A implements Runnable{
+//     public void run()
+//       {
+//         for(int i=0; i<=100; i++)
+//         {
+//             System.out.println("Hey");
+//             try
+//               {
+//                 Thread.sleep(500);
+//               }
+//               catch(InterruptedException e){ e.printStackTrace();
+
+//               }
+//         }
+//       }
+// }
+
+// class B implements Runnable{
+//     public void run(){
+//         for(int i= 0; i<=100; i++){
+//             System.out.println("Helli ji");
+//             try{
+//                 Thread.sleep(10);
+//             }
+//             catch(InterruptedException e){
+//                 e.printStackTrace();
+//             }
+//         }
+//     }
+// }    
+
+// public class RunnableThread {
+//     public static void main(String args[]){
+
+//         A obj1 = new A();
+//         B obj2 = new B();
+
+//         Thread t1 = new Thread(obj1); // Creating thread for A, it takes Runnable object as argument
+//         Thread t2 = new Thread(obj2);
+
+//         t1.start();
+//         t2.start();
+
+//     }
+    
+// }
+
+/***************************************************/
+/*********SAME CODE USING LAMDA EXPRESIION***********/
+
+
+public class RunnableThread {
+    public static void main(String args[]){
+
+        Runnable obj1 = () -> {
+            for(int i=0; i<=5; i++){
+                System.out.println("Hey");
+                 try{ Thread.sleep(10); }
+                 catch(InterruptedException e){ e.printStackTrace();  }
+            }
+        };
+
+        Runnable obj2 = () -> {
+            for(int i =0; i<=5; i++){
+                System.out.println("Hello ji");
+                    try{ Thread.sleep(10); }
+                    catch(InterruptedException e){ e.printStackTrace();  }
+            }
+        };
+
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+
+        obj1.run();
+        obj2.run();
+
+
+}
+}
